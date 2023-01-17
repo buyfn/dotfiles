a6e13a685dc4711593fd07adbbcbae884be0ac61
ignat <buyfng@gmail.com>
2023-01-17 14:34:50 +0100

    Set up eslint

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,18 +7,17 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(backup-directory-alist '(("." . "~/.emacs.d/backups")))
+ '(custom-enabled-themes '(solarized-dark))
  '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+   '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(inhibit-startup-screen t)
  '(initial-buffer-choice nil)
  '(js-indent-level 2)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (ledger-mode exec-path-from-shell js2-mode solarized-theme)))
+   '(flycheck js-comint xref-js2 js2-refactor magit racket-mode nim-mode sml-mode ledger-mode exec-path-from-shell js2-mode solarized-theme))
+ '(pascal-indent-level 2)
  '(scroll-bar-mode nil)
  '(sml-indent-level 2)
  '(tool-bar-mode nil))
@@ -27,4 +26,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180)))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "IBM Plex Mono")))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,9 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+
+;;; Code:
 (package-initialize)
 
 (require 'package)
@@ -11,11 +12,43 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(require 'js2-refactor)
+(require 'js-comint)
+(require 'flycheck)
+
 ; run js2-mode for js files
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(setq js2-skip-preprocessor-directives t)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(setq-default flycheck-temp-prefix ".flycheck")
+(setq-default flycheck-disabled-checkers
+	      (append flycheck-disabled-checkers
+		      '(javascript-jshint)))
+
+(defun js2-mode-use-eslint-indent ()
+  (let ((json-object-type 'hash-table)
+    (json-config (shell-command-to-string (format  "eslint --print-config %s"
+                               (shell-quote-argument
+                            (buffer-file-name))))))
+    (ignore-errors
+      (setq js-indent-level
+        (aref (gethash "indent" (gethash  "rules" (json-read-from-string json-config))) 1)))))
+
+(add-hook 'js2-mode-hook #'js2-mode-use-eslint-indent)
 
 ; load environment variables
 (exec-path-from-shell-initialize)
 
 ; Toggle fullscreen on Ctrl+return
 (global-set-key (kbd "C-<return>") 'toggle-frame-fullscreen)
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+; auto refresh in dired
+(add-hook 'dired-mode-hook 'auto-revert-mode)
+
+; spaces for tabs
+(setq-default indent-tabs-mode nil)
